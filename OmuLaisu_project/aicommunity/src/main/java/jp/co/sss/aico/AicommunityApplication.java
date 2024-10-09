1c4a014ad680d23d2ce357c2caf959cc2541c7ce
水野学 <vga29-pc230054@sankogakuen.jp>
2024-10-09 12:24:18 +0900

    プロジェクトを追加

---
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/sss/aico/AicommunityApplication.java
@@ -0,0 +1,13 @@
+package jp.co.sss.aico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicommunityApplication.class, args);
+	}
+
+}
---
+++ OmuLaisu_project/aicommunity/src/test/java/jp/co/sss/aico/AicommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.sss.aico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
