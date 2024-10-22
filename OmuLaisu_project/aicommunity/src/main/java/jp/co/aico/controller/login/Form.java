d5226d65f6f8b736da764e85fca1ba74289ffabe
イ　ファンギュ <vga29-pc230004@sankogakuen.jp>
2024-10-22 11:27:09 +0900

    xxx

---
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/login/Form.java
@@ -0,0 +1,5 @@
+package jp.co.aico.controller.login;
+
+public class Form {
+
+}
---
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/login/SessionController.java
@@ -0,0 +1,33 @@
+package jp.co.aico.controller.login;
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+
+public class SessionController {
+
+	@RequestMapping(path = "/loginOnSession", method = RequestMethod.GET)
+	public String loginOnSession() {
+		return "session/loginOnSession";
+	}
+
+	@RequestMapping(path = "/doLginOnSession", method = RequestMethod.POST)
+	public String doLoginOnSession(LoginForm form, HttpSession session) {
+		if (form.getUserId() == 123456) {
+			session.setAttribute("userId", form.getUserId());
+			return "redirect:/";
+
+		} else {
+			return "session/loginOnSession";
+		}
+	}
+
+	@RequestMapping(path = "/logout", method = RequestMethod.GET)
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
+}
