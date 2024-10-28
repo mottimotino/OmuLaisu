ae8befdce2813859e60f6242ee36d83507519457
村 <vga29-pc230056@sankogakuen.jp>
2024-10-28 15:20:40 +0900

    登録コントローラー完了

--- OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/registration/RegistationController.java
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/registration/RegistationController.java
@@ -2,11 +2,18 @@ package jp.co.aico.controller.registration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.co.aico.entity.UsersEntity;
+import jp.co.aico.form.UsersForm;
+import jp.co.aico.repository.UsersRepository;
 
 @Controller
 public class RegistationController {
 @Autowired
+UsersRepository repository;
 /**
  * /RepositoryをAutowiredの下にインスタンス化してください
  * 入力ホーム
@@ -20,10 +27,20 @@ public String registForm() {
 /**
  * 確認ホーム
  * 村越
+ * returnで/user/regist/checkに遷移
  */
 @RequestMapping("/user/regist/check")
-public String registCheck() {
-	return "";
-	
+public String registCheck(Model model,UsersForm form) {
+	model.addAttribute("Users",form);
+	return "/user/regist/check";
+}
+//完了画面
+@RequestMapping(path="/user/regist/complete",method=RequestMethod.POST)
+public String registComplete(UsersForm form) {
+	UsersEntity Entity = new UsersEntity();
+	Entity.setName(form.getName());
+	Entity.setPassword(form.getPassword());
+	 repository.save(Entity);
+	return "/user/regist/complete";
 }
 }
