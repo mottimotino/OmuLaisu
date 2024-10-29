b3f11fda88301a2162fa51e13a4351d8d1b97bbe
mottimotino <vga29-pc230033@sankogakuen.jp>
2024-10-29 12:02:03 +0900

    競合解決用　鈴木

--- OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/login/LoginController.java
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/login/LoginController.java
@@ -2,16 +2,27 @@ package jp.co.aico.controller.login;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.servlet.http.HttpSession;
+import jp.co.aico.entity.UsersEntity;
 import jp.co.aico.form.UsersForm;
+import jp.co.aico.repository.UsersRepository;
 
+/**
+ * ログイン機能のコントローラー
+ * @author 水野
+ *
+ */
 public class LoginController {
 
 //	@Autowired
 //	UsersRepository;
+	//会員テーブルのリポジトリ
+	@Autowired
+	UsersRepository usersRepository;
 	/**
 	 * ログイン画面を表示
 	 * @return login内のlogin.html
@@ -21,22 +32,38 @@ public class LoginController {
 		return "login/login";
 	}
 
-	@RequestMapping(path = "/doLogin", method = RequestMethod.POST)
+	@RequestMapping(path = "/doLgin", method = RequestMethod.POST)
 	public String doLoginOnSession(UsersForm form, HttpSession session) {
+	/**
+	 * ログイン認証
+	 * @param form 入力されたメールアドレス,パスワード
+	 * @param session メールアドレス,パスワードを保存
+	 * @return top内のdotop.html/login内のlogin.html
+	 */
+	@RequestMapping(path = "/doLogin", method = RequestMethod.POST)
+	public String doLogin(UsersForm form, HttpSession session) {
 		//入力されたメールアドレスを受け取る
 		String mail = form.getMail();
 		//入力パスワードを受け取る
 		String password = form.getPassword();
+		//入力された内容で条件検索
+		UsersEntity users = usersRepository.findByMailAndPassword(mail, password);
 		
 		
 		
 		return "";
-		
-	}
-	@RequestMapping(path = "/doLogin",method = RequestMethod.POST)
-	public String logout(HttpSession session) {
-		session.invalidate();
-		return "top:/";
+		//ログインに成功
+		if(users != null) {
+			//入力内容をsessionで保存
+			session.setAttribute("mail", mail);
+			session.setAttribute("password", password);
+			//ログイン後トップ画面に遷移
+			return "top/dotop";
+		//ログインに失敗
+		} else {
+			//入力画面に遷移
+			return "login/login";
+		}
 	}
 
 }
--- OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/repository/UsersRepository.java
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/repository/UsersRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import jp.co.aico.entity.UsersEntity;
 
 public interface UsersRepository extends JpaRepository<UsersEntity,Integer>{
+	
+	UsersEntity findByMailAndPassword(String mail,String password);
 
 }
