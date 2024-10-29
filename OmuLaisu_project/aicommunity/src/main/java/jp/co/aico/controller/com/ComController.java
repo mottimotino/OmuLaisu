c9694231d4664bf346e2d2f8277df4d22d00b791
mottimotino <vga29-pc230033@sankogakuen.jp>
2024-10-29 12:36:50 +0900

    会話予約　鈴木

--- OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/com/ComController.java
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/com/ComController.java
@@ -1,8 +1,32 @@
 package jp.co.aico.controller.com;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 会話予約
+ * @author 鈴木
+ *
+ */
 @Controller
 public class ComController {
+	
+	@RequestMapping("/calender")
+	//scheduleReservation→日程予約 名前短縮してscheReserv
+	public String scheReserv() {
+		return "";
+	}
+	
+	@RequestMapping("/calender")
+	//scheduleReservationConfirmation→日程予約確認 名前短縮してscheReservConfir
+	public String scheReservConfir() {
+		return "";
+	}
+	
+	@RequestMapping("/calender")
+	//scheduleReservationCompleted→日程予約完了 名前短縮してscheReservComp
+	public String scheReservComp() {
+		return "";
+	}
 
 }
