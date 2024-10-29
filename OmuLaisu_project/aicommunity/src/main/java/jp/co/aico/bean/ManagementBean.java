588d291b2085bb2e1564d8990aa10c66077dc607
村 <vga29-pc230056@sankogakuen.jp>
2024-10-29 12:35:37 +0900

    パスワード再設定コントローラー設定

---
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/bean/ManagementBean.java
@@ -0,0 +1,25 @@
+package jp.co.aico.bean;
+public class ManagementBean {
+private Integer manaId;
+private Integer prizeId;
+private Integer usersId;
+public Integer getManaId() {
+	return manaId;
+}
+public void setManaId(Integer manaId) {
+	this.manaId = manaId;
+}
+public Integer getPrizeId() {
+	return prizeId;
+}
+public void setPrizeId(Integer prizeId) {
+	this.prizeId = prizeId;
+}
+public Integer getUsersId() {
+	return usersId;
+}
+public void setUsersId(Integer usersId) {
+	this.usersId = usersId;
+}
+
+}
--- OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/management/ManagementController.java
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/management/ManagementController.java
@@ -1,8 +1,54 @@
 package jp.co.aico.controller.management;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.aico.bean.ManagementBean;
+import jp.co.aico.entity.ManagementEntity;
+import jp.co.aico.form.Managementform;
+import jp.co.aico.repository.ManagementRepository;
 
 @Controller
 public class ManagementController {
+	@Autowired
+	ManagementRepository repository;
+	/**
+	 * 入力フォーム
+	 * @param manaId
+	 * @param model
+	 * @return
+	 * 
+	 */
+	@RequestMapping("user/update/input/{manaId}")
+	public String kousin(@PathVariable Integer manaId, Model model) {
+		ManagementEntity Entity = repository.getReferenceById(manaId);
+		ManagementBean bean = new ManagementBean();
+		BeanUtils.copyProperties(Entity, bean);
+		model.addAttribute("up", bean);
+		return "";
 
+	}
+	@RequestMapping()
+	public String check(Managementform form) {
+		
+		return null;
+		
+}
+/**
+ * 更新完了処理
+ * @param manaId
+ * @param Managementform
+ * @return
+ */
+	@RequestMapping("user/update/complete/{manaId}")
+	public String update(@PathVariable Integer manaId ,  Managementform form) {
+		 ManagementEntity Entity = repository.getReferenceById(manaId);
+		BeanUtils.copyProperties(form, Entity, "manaId");
+		Entity = repository.save(Entity);
+		return "";
+	}
 }
