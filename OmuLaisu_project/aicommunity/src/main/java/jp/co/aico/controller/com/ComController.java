2854e2452125110f43dfe76cd48dfb996305ca77
mottimotino <vga29-pc230033@sankogakuen.jp>
2024-10-31 10:16:54 +0900

    競合　鈴木

--- OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/com/ComController.java
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/com/ComController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * 会話予約
  * @author 鈴木
  *
+ *html名
+ *日程選択:view
+ *選択確認:check
+ *予約完了:input
  */
 @Controller
 public class ComController {
