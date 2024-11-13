a5a10df1f734f1c765941a4a7d6c261e3cbe9641
村 <vga29-pc230056@sankogakuen.jp>
2024-11-13 12:41:39 +0900

    Controller作成途中

--- OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/com/ComController.java
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/com/ComController.java
@@ -1,7 +1,15 @@
 package jp.co.aico.controller.com;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.aico.entity.TimesEntity;
+import jp.co.aico.repository.ReservationRepository;
+import jp.co.aico.repository.TimesRepository;
 
 /**
  * 会話予約
@@ -14,8 +22,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class ComController {
+@Autowired
+private  ReservationRepository reservationRepository;
+@Autowired
+private TimesRepository timesRepository;
+/**
+ * @author 村越
+ * @return　view
+ * カレンダーの日程を全件検索で表示するメソット
+ */
 //カレンダー機能
-public String carendar() {
-return "";
+@RequestMapping("allViews")
+public String views(Model model) {
+	List<TimesEntity> timesAll = timesRepository.findAll();
+	model.addAttribute("timesAll", timesAll);
+    return "calendar/view";
+}
+@RequestMapping("reservationCheck")
+public String Checks(Model model){
+    Optional<TimesEntity> timesCheck = timesRepository.findById(reservationRequest.().gettimesId());
+
+	return"calendar/check";
 }
 }
