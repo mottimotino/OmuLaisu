49d729e87b6e917ddb465b8f5f674f2b78d5db4c
水野学 <vga29-pc230054@sankogakuen.jp>
2024-11-18 15:11:37 +0900

    entity/AccuracyEntity.java　追加

---
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/form/QuizForm.java
@@ -0,0 +1,47 @@
+package jp.co.aico.form;
+
+/**
+ * クイズ機能のform
+ * @author 水野学
+ *
+ */
+public class QuizForm {
+	
+	/** 問題ID */
+	private Integer queId;
+	/** 選択した番号 */
+	private  Integer answer;
+	/** 問題のジャンル */
+	private Integer categoryType;
+
+	public Integer getQueId() {
+		return queId;
+	}
+
+	public void setQueId(Integer queId) {
+		this.queId = queId;
+	}
+
+	public Integer getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(Integer answer) {
+		this.answer = answer;
+	}
+
+	public Integer getCategoryType() {
+		return categoryType;
+	}
+
+	public void setCategoryType(Integer categoryType) {
+		this.categoryType = categoryType;
+	}
+
+	
+	
+	
+	
+	
+
+}
