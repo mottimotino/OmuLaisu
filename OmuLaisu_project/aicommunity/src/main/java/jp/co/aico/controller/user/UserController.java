313d8b9c1f64d0eeef1132a1ebf2a2689f52e1d9
mottimotino <vga29-pc230033@sankogakuen.jp>
2024-11-25 14:30:52 +0900

    サイドバー　画面、コントローラー作成　鈴木

--- OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/user/UserController.java
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/user/UserController.java
@@ -1,8 +1,43 @@
 package jp.co.aico.controller.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
+	
+	//ログイン中サイドバー内画面遷移(一旦基本機能のみ)
+	
+	//日程予約
+	@RequestMapping("/calender/view")
+	public String view() {
+		return "calender/view";
+	}
+	
+	//クイズ(quizコントローラーから持ってきた)
+	@RequestMapping(path = "/quiz/list", method = RequestMethod.GET)
+	public String quiz_list() {
+		return "quiz/list";
+	}
+	
+	//マイページ
+	@RequestMapping(path = "/user/info", method = RequestMethod.GET)
+	public String mypage() {
+		return "user/info";
+	}
+	
+	//Topに戻る(命名どうする？)
+	@RequestMapping(path = "/top/dotop", method = RequestMethod.GET)
+	public String loginAfter() {
+		return "top/dotop";
+		
+	}
+	
+	//追加機能コントローラー
+	//日程予約リンク表示
+	//チャット
+	//問い合わせ
+	
 
 }
