bd04fb24db75580118c91dcaa4348498abcf369c
村 <vga29-pc230056@sankogakuen.jp>
2024-11-26 12:48:05 +0900

    form,コントローラー作成

---
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/form/ComForm.java
@@ -0,0 +1,45 @@
+package jp.co.aico.form;
+
+import java.util.Date;
+public class ComForm {
+
+private Integer dateId;
+private Date day;
+private String weekday;
+private Integer timesId;
+private  Integer usersId;
+public Integer getDateId() {
+	return dateId;
+}
+public void setDateId(Integer dateId) {
+	this.dateId = dateId;
+}
+public Date getDay() {
+	return day;
+}
+public void setDay(Date day) {
+	this.day = day;
+}
+public String getWeekday() {
+	return weekday;
+}
+public void setWeekday(String weekday) {
+	this.weekday = weekday;
+}
+public Integer getTimesId() {
+	return timesId;
+}
+public void setTimesId(Integer timesId) {
+	this.timesId = timesId;
+}
+public Integer getUsersId() {
+	return usersId;
+}
+public void setUsersId(Integer usersId) {
+	this.usersId = usersId;
+}
+
+
+
+
+}
