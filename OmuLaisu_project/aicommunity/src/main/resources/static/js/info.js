ba39364fcf9aeb4036e67e90ce4b9754a9e5cec9
mottimotino <vga29-pc230033@sankogakuen.jp>
2024-12-24 18:04:27 +0900

    円グラフ　修正中　鈴木

--- OmuLaisu_project/aicommunity/src/main/resources/static/js/info.js
+++ OmuLaisu_project/aicommunity/src/main/resources/static/js/info.js
@@ -1,16 +1,38 @@
-var ctx = document.getElementById('myChart');
-var myDoughnutChart = new Chart(ctx,{
-	type:'doughnut',
-	data:{
-		labels:['正解','不正解'],
-		datasets: [
-			{
-			data:[50,50],
-			backgroundColor:["#f66","#0000ff"]
-			}
-		]
-	}
-});
+//var ctx = document.getElementById('myChart');
+//var myDoughnutChart = new Chart(ctx,{
+//	type:'doughnut',
+//	data:{
+//		labels:["いちご","ぶどう","バナナ"],
+//		datasets: [
+//		{
+//			data:[300,100,80],
+//			backgroundColor:["#f66","#c7e","#fc2"]
+//			}
+//		]
+//	}
+//});
+
+	const ctx = document.getElementById('myChart')
+
+//この2つの値を設定するだけ!!
+const percentage = 76 //グラフのパーセンテージ(76%)
+const backgroundColor = 'rgba(255, 0, 0, 1)' //グラフの色(赤)
+
+new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    datasets: [{
+      data: [percentage, 100 - percentage],
+      backgroundColor: [
+        backgroundColor, //赤色
+        'rgba(0, 0, 0, 0)',
+      ],
+      borderWidth: 0
+    }]
+  }
+})
+
+
 
 
 //var pieData = {
