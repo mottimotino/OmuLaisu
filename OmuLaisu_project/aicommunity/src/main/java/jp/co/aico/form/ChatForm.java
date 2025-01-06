9dc8bd933c179d8f2beeb64b433a34ebbb65ad90
水野学 <vga29-pc230054@sankogakuen.jp>
2025-01-06 10:24:33 +0900

    ChatForm.java作成,chat.css作成

---
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/form/ChatForm.java
@@ -0,0 +1,41 @@
+package jp.co.aico.form;
+
+public class ChatForm {
+	
+	private Integer sendUser;
+	
+	private Integer receUser;
+	
+	private String message;
+
+	public Integer getSendUser() {
+		return sendUser;
+	}
+
+	public void setSendUser(Integer sendUser) {
+		this.sendUser = sendUser;
+	}
+
+	public Integer getReceUser() {
+		return receUser;
+	}
+
+	public void setReceUser(Integer receUser) {
+		this.receUser = receUser;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	
+	
+	
+	
+	
+
+}
