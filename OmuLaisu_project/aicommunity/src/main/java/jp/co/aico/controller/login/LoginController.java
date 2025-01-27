7761a73c7e7510cbdda3ef663952a416e0cb174a
mottimotino <vga29-pc230033@sankogakuen.jp>
2025-01-27 14:37:46 +0900

    ログイン後ヘッダー変更 LoginController 追記 鈴木

--- OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/login/LoginController.java
+++ OmuLaisu_project/aicommunity/src/main/java/jp/co/aico/controller/login/LoginController.java
@@ -1,7 +1,5 @@
 package jp.co.aico.controller.login;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,14 +20,15 @@ import jp.co.aico.repository.UsersRepository;
 @Controller
 public class LoginController {
 
-//	//会員テーブルのリポジトリ
+	//	//会員テーブルのリポジトリ
 	@Autowired
-	UsersRepository usersRepository;	
-	
+	UsersRepository usersRepository;
+
 	@RequestMapping(path = "/top2", method = RequestMethod.GET)
 	public String top2() {
 		return "top/top2";
 	}
+
 	/**
 	 * ログイン画面を表示
 	 * @return login内のlogin.html
@@ -39,13 +38,13 @@ public class LoginController {
 		return "login/login";
 	}
 
-/**
-	 * ログイン認証
-	 * @param form 入力されたメールアドレス,パスワード
-	 * @param session メールアドレス,パスワードを保存
-	 * @return top内のdotop.html/login内のlogin.html
-	 */	
-	@RequestMapping(path = "/doLogin", method = RequestMethod.POST)
+	/**
+		 * ログイン認証
+		 * @param form 入力されたメールアドレス,パスワード
+		 * @param session メールアドレス,パスワードを保存
+		 * @return top内のdotop.html/login内のlogin.html
+		 */
+	@RequestMapping(path = "top/dotop", method = RequestMethod.POST)
 	public String doLogin(UsersForm form, HttpSession session) {
 		//入力されたメールアドレスを受け取る
 		String mail = form.getMail();
@@ -53,34 +52,34 @@ public class LoginController {
 		String password = form.getPassword();
 		//入力された内容で条件検索
 		UsersEntity users = usersRepository.findByMailAndPassword(mail, password);
-		
-	//ログインに成功
-		if(users != null) {
+
+		//ログインに成功
+		if (users != null) {
 			//入力内容をsessionで保存
 			session.setAttribute("usersId", users.getUsersId());
 			session.setAttribute("mail", mail);
 			session.setAttribute("password", password);
 			//ログイン後トップ画面に遷移
 			return "top/dotop";
-		//ログインに失敗
+			//ログインに失敗
 		} else {
 			//入力画面に遷移
 			return "login/login";
-		}	}
-	@RequestMapping(path = "/doLogin",method = RequestMethod.GET)
+		}
+	}
+
+	@RequestMapping(path = "/doLogin", method = RequestMethod.GET)
 	public String logout(HttpSession session) {
 		session.invalidate();
-		return "top:/";
+		return "redirect:/";
 	}
 	//マネジメントコントローラーに記載
-//	//パスワード再設定画面への遷移
-//	@RequestMapping("user/update/input")
-//	public String passwordChange() {
-//		return "user/update/input";
-//	}
-	
-	
-	
+	//	//パスワード再設定画面への遷移
+	//	@RequestMapping("user/update/input")
+	//	public String passwordChange() {
+	//		return "user/update/input";
+	//	}
+
 	/**
 	 * ▼▼ここからパスワードを忘れた場合のパスワード変更
 	 * メールアドレス確認
@@ -89,22 +88,22 @@ public class LoginController {
 	 * @return input
 	 * 
 	 */
-//	@RequestMapping("user/update/input/{usersId}")
-//	public String kousin(@PathVariable Integer usersId, Model model) {
-//		UsersEntity Entity = usersRepository.getReferenceById(usersId);
-//		model.addAttribute("oldPassword", Entity);
-//		return "user/update/input";
-//
-//	}
-	
+	//	@RequestMapping("user/update/input/{usersId}")
+	//	public String kousin(@PathVariable Integer usersId, Model model) {
+	//		UsersEntity Entity = usersRepository.getReferenceById(usersId);
+	//		model.addAttribute("oldPassword", Entity);
+	//		return "user/update/input";
+	//
+	//	}
+
 	@RequestMapping("/login/mailCheck")
 	public String mailCheck() {
-//	public String mailCheck(@PathVariable String select,HttpSession session) {
-//		session.setAttribute("select", select);
+		//	public String mailCheck(@PathVariable String select,HttpSession session) {
+		//		session.setAttribute("select", select);
 		return "login/mailCheck";
 	}
-	
-//	▲▲メールアドレス確認
+
+	//	▲▲メールアドレス確認
 
 	/**
 	 *パスワード変更
@@ -113,17 +112,17 @@ public class LoginController {
 	 * @param model
 	 * @return check
 	 */
-	@RequestMapping(path="/login/passwordForget",method=RequestMethod.POST)
-	public String Forgetpass(UsersForm form, Model model) {
+	@RequestMapping(path = "/login/passwordForget", method = RequestMethod.POST)
+	public String Forgetpass(UsersForm form, Model model, HttpSession session) {
 		UsersEntity usersEntity = usersRepository.findByMail(form.getMail());
-		
-	if(form.getMail().equals(usersEntity.getMail())){
-		model.addAttribute("mail", form.getMail());
-		model.addAttribute("user",usersEntity);
-		
-		return "login/passwordForget";
-	} else{
-		return "user/mailCheck";
+
+		if (form.getMail().equals(usersEntity.getMail())) {
+			model.addAttribute("mail", form.getMail());
+			session.setAttribute("user", usersEntity);
+
+			return "login/passwordForget";
+		} else {
+			return "user/mailCheck";
 		}
 
 	}
@@ -139,26 +138,21 @@ public class LoginController {
 	 * メアドで検索→パスワードを上書き
 	 */
 	@RequestMapping(path = "/login/passwordCommit", method = RequestMethod.POST)
-	public String PassChange(String password,UsersEntity user) {
-//		public String PassChange(UsersForm form, Model model) {
-//		これで実行できるか確認→実行できないとパスワード以外はnullで更新するといった行動になってしまっている。
-//		UsersEntity usersEntity = usersRepository.findByMailAndPassword(form.getMail(),form.getPassword());
-//		BeanUtils.copyProperties(form, user, "usersId");
-		System.out.println(user.getName());
-		user.setPassword(password);
-		usersRepository.save(user);
-//		UsersBean usersBean = new UsersBean();
-//		BeanUtils.copyProperties(usersEntity, usersBean);
-//		model.addAttribute("users", usersBean);
+	public String PassChange(String password, UsersForm form, HttpSession session) {
+		//		public String PassChange(UsersForm form, Model model) {
+		UsersEntity users = (UsersEntity) session.getAttribute("user");
+		users.setPassword(password);
+		usersRepository.save(users);
+
 		return "login/passwordCommit";
 	}
-	
+
 	@RequestMapping("/login")
-	public String passCommit(@PathVariable String select,HttpSession session) {
+	public String passCommit(@PathVariable String select, HttpSession session) {
 		session.setAttribute("select", select);
 		return "login/login";
 	}
-	
-//	パスワード変更ここまで▲▲
+
+	//	パスワード変更ここまで▲▲
 
 }
