dcb470030425fb44273c688f8136c9312199db3e
水野学 <vga29-pc230054@sankogakuen.jp>
2025-02-13 18:04:13 +0900

    クイズ諸々

--- OmuLaisu_project/aicommunity/src/main/resources/static/js/quiz.js
+++ OmuLaisu_project/aicommunity/src/main/resources/static/js/quiz.js
@@ -1,3 +1,18 @@
 /**
  * クイズ機能のjs
  */
+
+/*クリックしたときに関数を実行*/
+
+
+function answerShow2(submit){
+	var answer = document.getElementById("answer").value;
+	var select = submit.value;
+	if(answer == select){
+		window.alert("○")
+	} else{
+		window.alert("×")
+	}
+	
+	
+}
